0fafbb20739e7467398c1c4a352bf0a97c53e4a5
Evan Merlock <vnmrlck@gmail.com>
2017-04-26 10:24:38 -0500

    Added an InputInfo structure to communicate the necessary codec information to the server

--- src/unsafe_code/input/mod.rs
+++ src/unsafe_code/input/mod.rs
@@ -1,5 +1,7 @@
 mod open_input;
 mod read_input;
+mod input_info;
 
 pub use self::open_input::*;
 pub use self::read_input::*;
+pub use self::input_info::*;
--- src/unsafe_code/input/open_input.rs
+++ src/unsafe_code/input/open_input.rs
@@ -1,9 +1,11 @@
+use unsafe_code::input::InputInfo;
 use unsafe_code::{UnsafeError, UnsafeErrorKind};
 
 use ffmpeg_sys::*;
 
 use std::ptr;
 use std::ffi::CString;
+use std::slice::from_raw_parts;
 
 unsafe fn allocate_format_context(input_format: &AVInputFormat, input_location: CString) -> Result<*mut AVFormatContext, UnsafeError> {
     let mut input_context_ptr = ptr::null_mut();
@@ -35,5 +37,25 @@ unsafe fn allocate_input_format(format_name: CString) -> *const AVInputFormat {
 pub fn create_input_format<'a>(format_name: CString) -> &'a AVInputFormat {
     unsafe {
         &*allocate_input_format(format_name)
+    }
+}
+
+unsafe fn allocate_input_information(format: &AVFormatContext) -> InputInfo {
+    let input_streams = from_raw_parts((*format).streams, (*format).nb_streams as usize);
+    let main_input_stream = input_streams[0];
+    let time_base = (*main_input_stream).time_base;
+
+    let codec = (*main_input_stream).codec;
+    let width = (*codec).width;
+    let height = (*codec).height;
+    let frame_rate = (*codec).framerate;
+
+    InputInfo::new(height, width, time_base.num, time_base.den, frame_rate.num, time_base.den)
+
+}
+
+pub fn create_input_information(format: &AVFormatContext) -> InputInfo {
+    unsafe {
+        allocate_input_information(format)
     }
 }

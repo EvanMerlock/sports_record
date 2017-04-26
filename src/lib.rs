0a3eb637c853982a6a1ada892d05c71d795ad607
Evan Merlock <vnmrlck@gmail.com>
2017-04-26 09:23:50 -0500

    Added Serde into lib crate

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,14 @@
+#[macro_use]
+extern crate serde_derive;
+
 extern crate iron;
 extern crate router;
 extern crate rusqlite;
 extern crate ffmpeg_sys;
 extern crate uuid;
 extern crate magick_rust;
+extern crate serde;
+extern crate serde_json;
 
 pub mod server;
 pub mod client;
